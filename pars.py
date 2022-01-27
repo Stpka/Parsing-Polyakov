edccd5c0460a2d631454ec2960b756da2f4a65b5
Stpka <77266906+Stpka@users.noreply.github.com>
2022-01-27 23:17:26 +0300

    Make easy functions for generate tasks and sample for parsing images

--- pars.py
+++ pars.py
@@ -1,22 +1,80 @@
 import requests
 from bs4 import BeautifulSoup
+import requests
 
+
+
+p = requests.get("https://kpolyakov.spb.ru/cms/images/4841.gif")
+out = open("test.html", "wb")
+out.write(p.content)
+out.close()
+
+
+tasks=[  [],
+		 [],
+		 [],
+
+		 [[158,-23],
+		 [169, "Базы данных: поиск в электронной таблице"]],
+
+		 [[110,-23],
+         [21, "Сокращение двоичного кода"],
+		 [22, "Выбор кода для одной буквы"],
+		 [23, "Помехоустойчивые коды"],
+		 [25, "Выбор кодов для нескольких букв"],
+		 [166, "Декодирование. Условие Фано"]],
+
+		 [[110,-23],
+		 [27, "Автомат для работы с числами"],
+		 [28, "Автомат с битами чётности"],
+		 [144,"Автомат с инверсией битовой записи"]],
+
+		 [],
+
+		 [[110,-23],
+		 [38, "Информационный объём изображений"],
+		 [39, "Информационный объём звуковых данных"]],
+
+		 [[110, -23],
+		 [42, "Анализ списка слов заданной длины"],
+		 [43, "Сколько слов можно составить при заданных ограничениях?"],
+		 [145, "Сколько чисел можно составить при заданных ограничениях?"]]
+
+		  ]
 questions = []
 answers=[]
-url = 'https://kpolyakov.spb.ru/school/ege/gen.php?action=viewAllEgeNo&egeId=4&cat21=on&cat22=on&cat23=on'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
 
 
-questions = soup.findAll('td', class_='topicview')
-answers = soup.findAll('div', class_='hidedata')
+def GenerateTasks(number_of_task, section):
+	url = f"https://kpolyakov.spb.ru/school/ege/gen.php?action=viewAllEgeNo&egeId={number_of_task}&cat{section}=on"
+	print(url)
+	return url
 
-def anqe(number):
-	print(str(questions[number])[110:-23])
+
+def setup():
+	task = int(input("Какой номер вы хотите проработать?: "))
+	left_slice=tasks[task][0][0]	# Генерируем срезы для этого номера
+	right_slice=tasks[task][0][1]
+
+	print(tasks[task])
+	number_of_section = int(input("Введите номер раздела: "))
+	page = requests.get(GenerateTasks(task,number_of_section)) 	# Создаём страницу
+	
+	soup = BeautifulSoup(page.text, "html.parser")				# Начинаем парсить
+	questions = soup.findAll('td', class_='topicview')
+	answers = soup.findAll('div', class_='hidedata')
+
+	for number in range(int(input("Сколько задач хотите сгенерировать?: "))):
+		AnswerAndQuestion(questions, answers, number, left_slice, right_slice)
+
+
+def AnswerAndQuestion(questions, answers, number, left_slice, right_slice):
+	print(str(questions[number])[left_slice:right_slice])
 	print()
 	print(str(answers[number])[80:-23])
 	print('=============')
 	print()
 
-for number in range(5):
-	anqe(number)
+
+setup()
+
