4a254eefa842d075e423793fc2a27ddec76dcf35
Stpka <77266906+Stpka@users.noreply.github.com>
2022-02-08 23:04:23 +0300

    Make func for files and start save media files in folders

--- pars.py
+++ pars.py
@@ -1,38 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import os
 
 tasks=[  [],
-		 [[108,-23],
-		 [12, "Оптимальный маршрут по весовой матрице"],
+		 [[12, "Оптимальный маршрут по весовой матрице"],
 		 [13, "Сопоставление вершин графа и весовой матрицы"]],
 		 
 		 [],
 
-		 [[110,-23],
-		 [169, "Базы данных: поиск в электронной таблице"]],
+		 [[169, "Базы данных: поиск в электронной таблице"]],
 
-		 [[109,-23],
-         [21, "Сокращение двоичного кода"],
+		 [[21, "Сокращение двоичного кода"],
 		 [22, "Выбор кода для одной буквы"],
 		 [23, "Помехоустойчивые коды"],
 		 [25, "Выбор кодов для нескольких букв"],
 		 [166, "Декодирование. Условие Фано"]],
 
-		 [[110,-23],
-		 [27, "Автомат для работы с числами"],
+		 [[27, "Автомат для работы с числами"],
 		 [28, "Автомат с битами чётности"],
 		 [144,"Автомат с инверсией битовой записи"]],
 
 		 [],
 
-		 [[110,-23],
-		 [38, "Информационный объём изображений"],
+		 [[38, "Информационный объём изображений"],
 		 [39, "Информационный объём звуковых данных"]],
 
-		 [[110, -23],
-		 [42, "Анализ списка слов заданной длины"],
+		 [[42, "Анализ списка слов заданной длины"],
 		 [43, "Сколько слов можно составить при заданных ограничениях?"],
 		 [145, "Сколько чисел можно составить при заданных ограничениях?"]]
 
@@ -42,27 +36,58 @@ questions = []
 answers=[]
 
 
+def find_file(string):
+	if re.search(r'<a href="ege-dbase/', string) != None:
+		resStart  = re.search(r"<a href=\"ege-dbase/", string).end()
+		resFinish = re.search(r".xls</a>", string).start()-6
+		out = str(string[resStart:resFinish]) 
+		url = f"https://kpolyakov.spb.ru/cms/files/ege-dbase/{out}"
+		get = requests.get(url)
+		outt = open(out, "wb")
+		outt.write(get.content)
+		outt.close()
+		os.replace(f"{out}", f"files/{out}")
+		return 1
+	else: print('Нет файла .xls')
 
-def find_thing(string):
-	if re.search(r"№&nbsp;", string).end() > 0: # Если в задаче есть картинка, то мы ее парсим
+
+def find_number_of_task(string):
+	if re.search(r"<img src=", string) != None: # Если в задаче есть картинка, то мы ее парсим
 
 		resStart  = re.search(r"№&nbsp;", string).end() # Берём начальные срезы для номера задачи
 		resFinish = re.search(r" ' ", string).start()-1
 		out = int(string[resStart:resFinish]) 			# Парсим номер зная срезы
 		return out
 
+	else: return False
+
+
+def find_text(string):
+	resStart  = re.search(r"changeImageFilePath", string).end()+2
+	resFinish = re.search(r"</script>", string).start()-8
+	out = string[resStart:resFinish] 
+	print('\n\n')	
+	return out
+
+
+def find_answer(string):
+	resStart  = re.search(r'changeImageFilePath', string).end()+2
+	resFinish = re.search(r"</script>", string).start()-8
+	out = string[resStart:resFinish]
+	return out
 
 
 def GenerateImg(number_of_task):
-	url = f'https://kpolyakov.spb.ru/cms/images/{number_of_task}.gif'
-	#print(url)
-	get = requests.get(url)
-
-	name_of_task = f"{number_of_task}.jpg"
-	out = open(name_of_task, "wb")
+	if number_of_task == False: return
 	
+	url = f'https://kpolyakov.spb.ru/cms/images/{number_of_task}.gif'
+	get = requests.get(url)
+	name_of_task = f"{number_of_task}.gif"
+	out = open(name_of_task, "wb")	
 	out.write(get.content)
 	out.close()
+
+	os.replace(f"{name_of_task}", f"img/{name_of_task}")
 	return name_of_task
 
 
@@ -75,46 +100,46 @@ def GenerateTasks(number_of_task, section):
 def setup():
 	STATE = 1 # Нормальное состояние
 
+	# Если папкк ещё не созданы
+	if not os.path.isdir("files") and not os.path.isdir("img"):
+		os.mkdir("files")
+		os.mkdir("img")
+
+
 	task = int(input("Какой номер вы хотите проработать?: "))
-	left_slice=tasks[task][0][0]	# Генерируем срезы для этого номера
-	right_slice=tasks[task][0][1]
-
 	print(tasks[task])
-	number_of_section = int(input("Введите номер раздела: "))
 
+	number_of_section = int(input("Введите номер раздела: "))
 	if number_of_section == 169:	STATE = 0
 	# Так как в этом разделе используются одна и таже каинка, то спрасим ее сразу здесь единажды
 
 	page = requests.get(GenerateTasks(task,number_of_section)) 	# Создаём страницу
-	
 	soup = BeautifulSoup(page.text, "html.parser")				# Начинаем парсить
 	questions = soup.findAll('td', class_='topicview')
 	answers = soup.findAll('div', class_='hidedata')
 	print(questions)
 
-
-
 	for number in range(int(input("Сколько задач хотите сгенерировать?: "))):
-		AnswerAndQuestion(questions, answers, number, left_slice, right_slice, STATE)
+		AnswerAndQuestion(questions, answers, number, STATE)
 
 
-def AnswerAndQuestion(questions, answers, number, left_slice, right_slice, STATE):
+def out(questions, answers, number):
+	find_file(str(questions[number]))
+	print(find_text(str(questions[number])))
+	print()
+	print(find_answer(str(answers[number])))
+	print('=============')	
+	print()
+
+
+
+def AnswerAndQuestion(questions, answers, number, STATE):
 	if STATE == 0 : 
 		GenerateImg(4282)
-		print(str(questions[number])[left_slice:right_slice])
-		print()
-		print(str(answers[number])[80:-23])
-		print('=============')
-		print()
-
+		out(questions, answers, number)
 	else:
-		GenerateImg(find_thing(str(questions[number])))
-
-		print(str(questions[number])[left_slice:right_slice])
-		print()
-		print(str(answers[number])[80:-23])
-		print('=============')
-		print()
+		GenerateImg(find_number_of_task(str(questions[number])))
+		out(questions, answers, number)
 
 
 setup()
